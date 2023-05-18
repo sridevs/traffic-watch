09424cce1cbca11b366db49830aad3bb15d0af26
sridevs <senthilnathansridev@gmail.com>
2023-05-18 19:02:48 +0800

    Added validation for dateString query param

--- traffic-watch-backend/src/controller/traffic-watch.controller.ts
+++ traffic-watch-backend/src/controller/traffic-watch.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
 import { TrafficWatchService } from '../service/traffic-watch.service';
 import { combineTrafficAndWeather } from '../util/location-util';
 import { TrafficWeather } from '../interface/traffic';
@@ -9,6 +9,11 @@ export class TrafficWatchController {
 
   @Get('')
   async getTrafficWeatherInfo(@Query('dateTime') dateTime: string): Promise<TrafficWeather[]> {
+    const dateTimeRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/;
+    const isValidDateTime = dateTimeRegex.test(dateTime);
+    if (!isValidDateTime) {
+      throw new BadRequestException('Invalid dateTime format. Expected format: YYYY-MM-DD[T]HH:mm:ss');
+    }
     const weatherForecasts = await this.trafficWatchService.getWeatherForecasts(dateTime);
     const trafficCameras = await this.trafficWatchService.getTrafficDetails(dateTime);
     return combineTrafficAndWeather(trafficCameras, weatherForecasts);
--- traffic-watch-backend/test/app.e2e-spec.ts
+++ traffic-watch-backend/test/app.e2e-spec.ts
@@ -2,14 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { TrafficWatchService } from '../src/service/traffic-watch.service';
 import { INestApplication } from '@nestjs/common';
-import { HttpModule, HttpService } from '@nestjs/axios';
-import axios, { AxiosResponse, InternalAxiosRequestConfig } from "axios";
+import { HttpModule } from '@nestjs/axios';
+import * as request from 'supertest';
 
 jest.mock('axios');
 describe('TrafficWatchController (e2e)', () => {
   let app: INestApplication;
   let trafficWatchService: TrafficWatchService;
-  let httpService: HttpService;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -17,10 +16,7 @@ describe('TrafficWatchController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    trafficWatchService = moduleFixture.get<TrafficWatchService>(
-      TrafficWatchService,
-    );
-    httpService = moduleFixture.get<HttpService>(HttpService);
+    trafficWatchService = moduleFixture.get<TrafficWatchService>(TrafficWatchService);
 
     await app.init();
   });
@@ -29,30 +25,75 @@ describe('TrafficWatchController (e2e)', () => {
     await app.close();
   });
 
-  describe('/traffic-watch', () => {
-    it('GET /traffic-watch should return traffic details', async () => {
-      const dateTime = '2023-05-16T12:00:00';
+  describe('GET /traffic-watch', () => {
+    it('should return combined traffic and weather information', async () => {
+      const dateTime = '2023-05-18T12:00:00';
+      const weatherForecasts = [
+        {
+          location: 'Singapore Central',
+          coordinate: { latitude: 1.3521, longitude: 103.8198 },
+          forecast: 'Partly cloudy',
+        },
+        {
+          location: 'Singapore East',
+          coordinate: { latitude: 1.3456, longitude: 103.983 },
+          forecast: 'Sunny',
+        },
+        {
+          location: 'Singapore West',
+          coordinate: { latitude: 1.3769, longitude: 103.7764 },
+          forecast: 'Rainy',
+        },
+      ];
+      const trafficCameras = [
+        {
+          camera_id: 'cam1',
+          image: 'image1.jpg',
+          image_metadata: { width: 1280, height: 720, md5: '12345' },
+          location: { latitude: 1.3521, longitude: 103.8198 },
+          timestamp: '2023-05-17T10:30:00Z',
+        },
+        {
+          camera_id: 'cam2',
+          image: 'image2.jpg',
+          image_metadata: { width: 1920, height: 1080, md5: '67890' },
+          location: { latitude: 1.2903, longitude: 103.8515 },
+          timestamp: '2023-05-17T10:35:00Z',
+        },
+      ];
+      const combinedData = [
+        {
+          camera_id: 'cam1',
+          image: 'image1.jpg',
+          image_metadata: { width: 1280, height: 720, md5: '12345' },
+          location: { latitude: 1.3521, longitude: 103.8198 },
+          locationName: 'Singapore Central',
+          weatherForecast: 'Partly cloudy',
+          timestamp: '2023-05-17T10:30:00Z',
+        },
+      ];
 
-      const expectedData = {
-        // Mocked response data
-        items: [],
-      };
+      jest.spyOn(trafficWatchService, 'getWeatherForecasts').mockResolvedValue(weatherForecasts);
 
-      const response: AxiosResponse = {
-        data: expectedData,
-        status: 200,
-        statusText: 'OK',
-        headers: {},
-        config: {} as InternalAxiosRequestConfig,
-      };
+      jest.spyOn(trafficWatchService, 'getTrafficDetails').mockResolvedValue(trafficCameras);
 
-      jest
-        .spyOn(axios, 'get')
-        .mockResolvedValue(response);
+      const response = await request(app.getHttpServer()).get('/traffic-watch').query({ dateTime }).expect(200);
 
-      const result = await trafficWatchService.getTrafficDetails(dateTime);
+      expect(trafficWatchService.getWeatherForecasts).toHaveBeenCalledWith(dateTime);
+      expect(trafficWatchService.getTrafficDetails).toHaveBeenCalledWith(dateTime);
+      expect(response.body).toEqual(combinedData);
+    });
 
-      expect(result).toEqual(expectedData.items);
+    it('should return 400 Bad Request for invalid dateTime format', async () => {
+      const invalidDateTime = '2023-05-18'; // Invalid format (missing time)
+      const errorMessage = 'Invalid dateTime format. Expected format: YYYY-MM-DD[T]HH:mm:ss';
+
+      const response = await request(app.getHttpServer())
+        .get('/traffic-watch')
+        .query({ dateTime: invalidDateTime })
+        .expect(400);
+
+      expect(response.body.message).toEqual(errorMessage);
     });
   });
 });
