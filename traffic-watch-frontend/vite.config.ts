89558be75970e64394596a0c6849a063dcf4f1c1
sridevs <senthilnathansridev@gmail.com>
2023-05-24 16:56:43 +0800

    Adding vite configs

---
+++ traffic-watch-frontend/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: "build",
+    },
+    plugins: [react()],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: 4000,
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setup.ts",
+    },
+  };
+});
